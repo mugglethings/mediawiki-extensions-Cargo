424fd616290209bd1d0f62b240756fe1944fb588
mugglethings <ljoshuacourt@outlook.com>
2025-11-17 21:48:31 +0000

    Updated to datatables 2.3.5

--- libs/ext.cargo.datatables.js
+++ libs/ext.cargo.datatables.js
@@ -1,116 +1,76 @@
 $(document).ready(function() {
-	$('.cargoDynamicTable').each( function( index ) {
-		var params = {};
-		var pageLength = $(this).attr( 'data-page-length' );
-		if ( pageLength != '' && pageLength > 0 && parseInt( pageLength ) == pageLength ) {
-			pageLength = parseInt( pageLength );
-			params['pageLength'] = pageLength;
-			var lengthOptions = [ 10, 25, 50, 100 ];
-			// If this is not one of the default options, add it
-			// to the list.
-			if ( lengthOptions.indexOf( pageLength ) < 0 ) {
-				lengthOptions.push( pageLength );
-				lengthOptions.sort( function(a, b){return a-b;} );
-				params['lengthMenu'] = lengthOptions;
+	$('.cargoDynamicTable').each(function(index) {
+
+		const $table = $(this);
+
+        let params = {
+            scrollX: true,
+            language: { search: "" },
+            columns: []
+        };
+
+		let pageLength = $table.attr('data-page-length');
+		const num = Number(pageLength);
+		if (Number.isInteger(num) && num > 0) {
+			params.pageLength = num;
+			let lengthOptions = [10, 25, 50, 100];
+			if (!lengthOptions.includes(num)) {
+                lengthOptions.push(num); 
+				lengthOptions.sort((a, b) => a - b);
+				params.lengthMenu = lengthOptions;
 			}
 		}
-		params[ 'aoColumns' ] = [];
-		var detailsFields = $(this).attr( 'data-details-fields' );
-		if ( detailsFields ) {
-			params['columnDefs'] = [{ "orderable":false, "targets": 0 }];
-			params[ 'aoColumns' ].push( { 'sWidth': '5px' } );
-		}
-		// Specify column widths if provided in display parameters
-		var columnWidths = $( this ).attr( 'data-widths' );
-		var columns  = [];
-		$( this ).find( 'th' ).each( function () {
-			if ( $( this ).text() !== "" )
-				columns.push( $( this ).text() );
-		} );
-		columns = [...new Set(columns)];
-		if ( columnWidths !== undefined ) {
-			columnWidths = columnWidths.split( ',' );
-			params[ 'bAutoWidth' ] = false;
-			columnWidths.forEach( function ( value ) {
-				columns.shift();
-				params[ 'aoColumns' ].push( {
-					'sWidth': value.trim()
-				} );
-			} );
-		}
-		while ( columns.length !== 0 ) {
-			params[ 'aoColumns' ].push( {
-				'sWidth': 'auto'
-			} );
-			columns.shift();
-		}
-		$( this ).attr( 'style', 'table-layout: fixed; word-wrap:break-word;' );
-		var table = $(this).DataTable( params );
 
-		// searchable columns
-		var tfoot = $(this).find('tfoot');
-		$(this).find('tfoot th').each( function () {
-			var placeholder = $(this).data('placeholder');
-			if ( placeholder ) {
-				$(this).html( '<input type="text" placeholder="'+placeholder+'"/>' );
-				tfoot.find('th').css('border-top', 'none');
-				tfoot.css('display', 'table-header-group');
-			}
-		} );
-		table.columns().every( function () {
-			var that = this;
-			$( 'input', this.footer() ).on( 'keyup change', function () {
-				if ( that.search() !== this.value ) {
-					that.search( this.value )
-						.draw();
-				}
-			} );
-		} );
+		const ths = $table.find('thead th');
+		const columnWidths = $table.attr('data-widths');
+		let widthList = [];
+		if(columnWidths) {
+			widthList = columnWidths.split(',').map(w => w.trim());
+		}
+		ths.each(function (i) {
+		    const colDef = {};
+		    if (widthList[i]) {
+		        colDef.width = widthList[i];
+		    } else {
+		        colDef.width = 'auto';
+		    }
+		    params.columns.push(colDef);
+		});
 
-		// hidden fields
-		$( 'a.toggle-vis' ).each( function () {
-			var column = table.column( $(this).data( 'column' ) );
-			column.visible( false );
-			$( this ).on( 'click', function ( e ) {
+		let dt = new DataTable(this, params);
+
+		$(dt.table().container())
+			.find('input[type="search"]')
+			.attr('placeholder', 'Search');
+
+		// hidden fields toggle
+		$('a.toggle-vis').each(function() {
+			let column = dt.column($(this).data('column'));
+			column.visible(false);
+			$(this).on('click', function(e) {
 				e.preventDefault();
-				column.visible( ! column.visible() );
-			} );
-		} );
-
-		// Add event listener for opening and closing details
-		$(this).find('tbody').on('click', 'td.details-control', function () {
-			var tr = $(this).closest('tr');
-			var row = table.row( tr );
-			if ( row.child.isShown() ) {
-				// This row is already open - close it
-				row.child.hide();
-				tr.removeClass('shown');
-			} else {
-				// Open this row
-				row.child( tr.data('details') ).show();
-				tr.addClass('shown');
-			}
-		} );
+				column.visible(!column.visible());
+			});
+		});
 
 		// Add popup tooltip for all column headers
-		var headerTooltips = $( this ).attr( 'data-tooltips' );
-		if ( headerTooltips !== undefined ) {
-			headerTooltips = headerTooltips.split( ',' );
-			$( 'th[aria-controls=DataTables_Table_' + index + ']' ).each( function ( idx ) {
-				if ( headerTooltips[idx] != undefined && headerTooltips[idx].trim() != '' ) {
-					var popupButton = new OO.ui.PopupButtonWidget( {
+		let headerTooltips = $table.attr('data-tooltips');
+		if (headerTooltips !== undefined) {
+			headerTooltips = headerTooltips.split(',');
+			$('th[aria-controls=DataTables_Table_' + index + ']').each(function(idx) {
+				if (headerTooltips[idx] && headerTooltips[idx].trim() !== '') {
+					let popupButton = new OO.ui.PopupButtonWidget({
 						icon: 'info',
 						framed: false,
 						popup: {
-							$content: $('<p>' + headerTooltips[ idx ].trim() + '</p>')
+							$content: $('<p>' + headerTooltips[idx].trim() + '</p>')
 						}
-					} );
-					$( this ).append( '&nbsp;' );
-					$( this ).append( popupButton.$element );
+					});
+					$(this).append('&nbsp;');
+					$(this).append(popupButton.$element);
 				}
-			} );
-			$( '.oo-ui-popupWidget-body p' ).attr( 'style', 'font-weight: normal;' );
+			});
+			$('.oo-ui-popupWidget-body p').attr('style', 'font-weight: normal;');
 		}
-	} );
-
-} );
+	});
+});
